f75713a089bb1cddaf1abd8f3efb6242b3d49cdb
Lukaaa <lucass.mellosilvar@gmail.com>
2024-06-07 13:02:10 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400,400);
+    background(random(0,255),random(0,255),random(0,255));
+  }
+  function draw() {
+      if (mouseIsPressed){
+        fill(random(0,255),random(0,255),random(0,255));
+        rect(mouseX,mouseY,20,20);
+      }
+  }
